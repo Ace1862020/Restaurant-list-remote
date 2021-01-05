526b9c39cae528b78187661e7d657bc117870025
Ace1862020 <kyds1862015@gmail.com>
2021-01-05 17:53:56 +0800

    feat: Add mongoose & create Model.

---
+++ models/restaurant.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const resranSchema = new Schema({
+  name: {
+    type: string,
+    required: true
+  },
+  name_en: string,
+  category: string,
+  image: string,
+  location: string,
+  phone: string,
+  google_map: string,
+  rating: string,
+  description: string
+})
+
+module.exports = mongoose.model('Resran', resranSchema)
