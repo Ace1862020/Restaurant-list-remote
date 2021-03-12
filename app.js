730550a9edb1ca4eafe38d0f74f160f46b7a0d3f
Ace1862020 <kyds1862015@gmail.com>
2021-03-12 11:28:57 +0800

    add express-session setting

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const port = 3000
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
@@ -13,6 +14,12 @@ const app = express()
 // set template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(session({
+  secret: 'ThisRestaurantSecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 // setting static files
 app.use(express.static('public'))
