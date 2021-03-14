f9ee6897ec7be7c90b2138f153ef70929b1aa84a
Ace1862020 <kyds1862015@gmail.com>
2021-03-14 19:19:47 +0800

    chore: heroku init

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 
-const db = mongoose.connection
+const MONGODB_URI = process.env.MONGODB_URI
 
 // Establish connection with mongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+
+const db = mongoose.connection
 
 db.on('error', () => { console.log('mongodb error!!') })
 db.once('open', () => { console.log('mongodb connected!') })
